a27ef81d59c8e6d153fd6d3a4fd28998b4694e7f
Tangent 128 <Tangent128@gmail.com>
2018-12-01 16:31:57 -0500

    elaborate wave

--- src/Level/Cave.ts
+++ src/Level/Cave.ts
@@ -4,6 +4,13 @@ import { SpawnStalacfite } from "Game/Enemy/Stalacfite";
 export class CaveLevel extends Level {
     constructor() {
         super();
-        this.addWave(new EvenPattern(40, SpawnStalacfite, SpawnStalacfite, SpawnStalacfite), 0);
+        this.addWave(new EvenPattern(40, SpawnStalacfite, SpawnStalacfite, SpawnStalacfite), 5);
+        this.addWave(new EvenPattern(100, SpawnStalacfite, SpawnStalacfite), 2);
+        this.addWave(new EvenPattern(40, SpawnStalacfite, SpawnStalacfite), 2);
+        this.addWave(new EvenPattern(40, SpawnStalacfite, SpawnStalacfite), 2);
+        this.addWave(new EvenPattern(40, SpawnStalacfite, SpawnStalacfite), 2);
+        this.addWave(new EvenPattern(50, SpawnStalacfite, SpawnStalacfite), 1);
+        this.addWave(new EvenPattern(60, SpawnStalacfite, SpawnStalacfite), 1);
+        this.addWave(new EvenPattern(40, SpawnStalacfite, SpawnStalacfite, SpawnStalacfite, SpawnStalacfite, SpawnStalacfite), 2);
     }
 }
