aa974ef3f58449670c4616baf3dbb7ce68c5fad8
Tangent 128 <Tangent128@gmail.com>
2018-12-01 12:24:02 -0500

    Don't allow piloting ship offscreen; scroll ship into view on spawn

--- src/Game/Player.ts
+++ src/Game/Player.ts
@@ -7,7 +7,8 @@ export function SpawnPlayer(data: Data, world: World): Id {
         playerShip: new PlayerShip(),
         location: new Location({
             X: world.width / 2,
-            Y: world.height - 15
+            Y: world.height + 15,
+            VY: -100
         }),
         bounds: new Polygon([
             -15, 10,
@@ -25,8 +26,26 @@ export function ControlPlayer(data: Data, world: World) {
     const diagonalSlowdown = (dx != 0 && dy != 0) ? 0.7 : 1;
 
     Join(data, "playerShip", "location").forEach(([id, ship, location]) => {
-        location.VX = dx * 300 * diagonalSlowdown;
-        location.VY = dy * 200 * diagonalSlowdown;
+        const overLeftEdge = location.X < 15;
+        const overRightEdge = location.X > (world.width - 15);
+        if((overLeftEdge && dx < 0) || (overRightEdge && dx > 0)) {
+            location.VX = 0;
+        } else {
+            location.VX = dx * 300 * diagonalSlowdown;
+        }
+
+        const overTopEdge = location.Y < 15;
+        const overBottomEdge = location.Y > (world.height - 20);
+        if(overTopEdge && dy < 0) {
+            location.VY = 0;
+        } else if(overBottomEdge && dy >= 0) {
+            // permit scroll onto screen
+            location.VY = Math.min(location.VY, 0);
+        } else {
+            location.VY = dy * 200 * diagonalSlowdown;
+        }
+
+        world.debug["Player VY"] = {overBottomEdge, overLeftEdge, overRightEdge, overTopEdge};
     });
 
     // edge-triggered
